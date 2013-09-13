71763b5bc61bd90e8d3be53f31c8ed98c96e2766
piratefsh <schong7@wisc.edu>
2013-09-13 16:48:01 -0500

    Add more partner cafe logos

--- js/main.js
+++ js/main.js
@@ -40,8 +40,8 @@ $(document).ready(function(){
     initializeTemplates(initializeResponsiveNav)
 
     //###################################################### GOOGLE ANALYTICS #####################################################
-    var _gaq=[['_setAccount','UA-43969265-1'],['_trackPageview']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
+    // var _gaq=[['_setAccount','UA-43969265-1'],['_trackPageview']];
+    // (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    // g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    // s.parentNode.insertBefore(g,s)}(document,'script'));
 })
